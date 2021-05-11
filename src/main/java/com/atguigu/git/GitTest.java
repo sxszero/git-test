ecc5f68532c12cc52013b36156a2240246ac2d58
kl150666 <1075424437@qq.com>
2021-05-11 09:33:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Auther: sxs
+ * @Date: 2021/5/11 09:28
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
