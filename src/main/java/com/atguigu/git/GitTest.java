5a61b330d22f8c3634c97621fdf19eb15d80aa26
kl150666 <44010524+kl150666@users.noreply.github.com>
2021-05-11 10:15:10 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hot-fix text!");
         System.out.println("true hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
